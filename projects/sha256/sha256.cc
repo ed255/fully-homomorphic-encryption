ae94d4189d4becb23f97e4b4634f8099c5a2a7f6
Eduard S <eduardsanou@posteo.net>
2024-07-25 00:13:02 +0000

    checkpoint

--- projects/sha256/sha256.cc
+++ projects/sha256/sha256.cc
@@ -27,20 +27,56 @@
 
 #include "sha256.h"
 
-void sha256_init(struct sha256_buff* buff) {
-    buff->h[0] = 0x6a09e667;
-    buff->h[1] = 0xbb67ae85;
-    buff->h[2] = 0x3c6ef372;
-    buff->h[3] = 0xa54ff53a;
-    buff->h[4] = 0x510e527f;
-    buff->h[5] = 0x9b05688c;
-    buff->h[6] = 0x1f83d9ab;
-    buff->h[7] = 0x5be0cd19;
-    buff->data_size = 0;
-    buff->chunk_size = 0;
+// struct sha256_buff buff;
+int  buff_data_size;
+int  buff_h[8];
+char buff_last_chunk[64];
+char buff_chunk_size;
+
+/* Initialization, must be called before any further use */
+void sha256_init();
+
+/* Process block of data of arbitary length, can be used on data streams (files, etc) */
+void sha256_update(const void* data, int size);
+
+/* Produces final hash values (digest) to be read
+   If the buffer is reused later, init must be called again */
+void sha256_finalize();
+
+/* Read digest into 32-byte binary array */
+void sha256_read(char* hash);
+
+void memcpy(char *dest, const char *src, int n) { 
+    // char *csrc = (char *)src; 
+    // char *cdest = (char *)dest; 
+    // Copy contents of src[] to dest[] 
+    #pragma hls_unroll yes
+    for (int i=0; i<n; i++) {
+        dest[i] = src[i]; 
+    } 
 }
 
-static const uint32_t k[64] = {
+void memset(char *dest, char ch, int n) { 
+    #pragma hls_unroll yes
+    for (int i=0; i<n; i++) {
+        dest[i] = ch; 
+    } 
+}
+
+void sha256_init() {
+    buff_h[0] = 0x6a09e667;
+    buff_h[1] = 0xbb67ae85;
+    buff_h[2] = 0x3c6ef372;
+    buff_h[3] = 0xa54ff53a;
+    buff_h[4] = 0x510e527f;
+    buff_h[5] = 0x9b05688c;
+    buff_h[6] = 0x1f83d9ab;
+    buff_h[7] = 0x5be0cd19;
+    buff_data_size = 0;
+    buff_chunk_size = 0;
+}
+
+static const int k[64] = {
     0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
     0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
     0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
@@ -53,36 +89,36 @@ static const uint32_t k[64] = {
 
 #define rotate_r(val, bits) (val >> bits | val << (32 - bits))
 
-static void sha256_calc_chunk(struct sha256_buff* buff, const uint8_t* chunk) {
-    uint32_t w[64];
-    uint32_t tv[8];
-    uint32_t i;
+static void sha256_calc_chunk(const char* chunk) {
+    int w[64];
+    int tv[8];
+    int i;
 
     #pragma hls_unroll yes
     for (i=0; i<16; ++i){
-        w[i] = (uint32_t) chunk[0] << 24 | (uint32_t) chunk[1] << 16 | (uint32_t) chunk[2] << 8 | (uint32_t) chunk[3];
+        w[i] = (int) chunk[0] << 24 | (int) chunk[1] << 16 | (int) chunk[2] << 8 | (int) chunk[3];
         chunk += 4;
     }
     
     #pragma hls_unroll yes
     for (i=16; i<64; ++i){
-        uint32_t s0 = rotate_r(w[i-15], 7) ^ rotate_r(w[i-15], 18) ^ (w[i-15] >> 3);
-        uint32_t s1 = rotate_r(w[i-2], 17) ^ rotate_r(w[i-2], 19) ^ (w[i-2] >> 10);
+        int s0 = rotate_r(w[i-15], 7) ^ rotate_r(w[i-15], 18) ^ (w[i-15] >> 3);
+        int s1 = rotate_r(w[i-2], 17) ^ rotate_r(w[i-2], 19) ^ (w[i-2] >> 10);
         w[i] = w[i-16] + s0 + w[i-7] + s1;
     }
     
     #pragma hls_unroll yes
     for (i = 0; i < 8; ++i)
-        tv[i] = buff->h[i];
+        tv[i] = buff_h[i];
     
     #pragma hls_unroll yes
     for (i=0; i<64; ++i){
-        uint32_t S1 = rotate_r(tv[4], 6) ^ rotate_r(tv[4], 11) ^ rotate_r(tv[4], 25);
-        uint32_t ch = (tv[4] & tv[5]) ^ (~tv[4] & tv[6]);
-        uint32_t temp1 = tv[7] + S1 + ch + k[i] + w[i];
-        uint32_t S0 = rotate_r(tv[0], 2) ^ rotate_r(tv[0], 13) ^ rotate_r(tv[0], 22);
-        uint32_t maj = (tv[0] & tv[1]) ^ (tv[0] & tv[2]) ^ (tv[1] & tv[2]);
-        uint32_t temp2 = S0 + maj;
+        int S1 = rotate_r(tv[4], 6) ^ rotate_r(tv[4], 11) ^ rotate_r(tv[4], 25);
+        int ch = (tv[4] & tv[5]) ^ (~tv[4] & tv[6]);
+        int temp1 = tv[7] + S1 + ch + k[i] + w[i];
+        int S0 = rotate_r(tv[0], 2) ^ rotate_r(tv[0], 13) ^ rotate_r(tv[0], 22);
+        int maj = (tv[0] & tv[1]) ^ (tv[0] & tv[2]) ^ (tv[1] & tv[2]);
+        int temp2 = S0 + maj;
         
         tv[7] = tv[6];
         tv[6] = tv[5];
@@ -96,106 +132,107 @@ static void sha256_calc_chunk(struct sha256_buff* buff, const uint8_t* chunk) {
 
     #pragma hls_unroll yes
     for (i = 0; i < 8; ++i)
-        buff->h[i] += tv[i];
+        buff_h[i] += tv[i];
 }
 
-void sha256_update(struct sha256_buff* buff, const void* data, size_t size) {
-    const uint8_t* ptr = (const uint8_t*)data;
-    buff->data_size += size;
+void sha256_update(const void* data, int size) {
+    const char* ptr = (const char*)data;
+    buff_data_size += size;
     /* If there is data left in buff, concatenate it to process as new chunk */
-    if (size + buff->chunk_size >= 64) {
-        uint8_t tmp_chunk[64];
-        memcpy(tmp_chunk, buff->last_chunk, buff->chunk_size);
-        memcpy(tmp_chunk + buff->chunk_size, ptr, 64 - buff->chunk_size);
-        ptr += (64 - buff->chunk_size);
-        size -= (64 - buff->chunk_size);
-        buff->chunk_size = 0;
-        sha256_calc_chunk(buff, tmp_chunk);
+    if (size + buff_chunk_size >= 64) {
+        char tmp_chunk[64];
+        memcpy(tmp_chunk, buff_last_chunk, buff_chunk_size);
+        memcpy(tmp_chunk + buff_chunk_size, ptr, 64 - buff_chunk_size);
+        ptr += (64 - buff_chunk_size);
+        size -= (64 - buff_chunk_size);
+        buff_chunk_size = 0;
+        sha256_calc_chunk(tmp_chunk);
     }
     /* Run over data chunks */
     #pragma hls_unroll yes
     while (size  >= 64) {
-        sha256_calc_chunk(buff, ptr);
+        sha256_calc_chunk(ptr);
         ptr += 64;
         size -= 64; 
     }
     
     /* Save remaining data in buff, will be reused on next call or finalize */
-    memcpy(buff->last_chunk + buff->chunk_size, ptr, size);
-    buff->chunk_size += size;
+    memcpy(buff_last_chunk + buff_chunk_size, ptr, size);
+    buff_chunk_size += size;
 }
 
-void sha256_finalize(struct sha256_buff* buff) {
-    buff->last_chunk[buff->chunk_size] = 0x80;
-    buff->chunk_size++;
-    memset(buff->last_chunk + buff->chunk_size, 0, 64 - buff->chunk_size);
+void sha256_finalize() {
+    buff_last_chunk[buff_chunk_size] = 0x80;
+    buff_chunk_size++;
+    memset(buff_last_chunk + buff_chunk_size, 0, 64 - buff_chunk_size);
 
     /* If there isn't enough space to fit int64, pad chunk with zeroes and prepare next chunk */
-    if (buff->chunk_size > 56) {
-        sha256_calc_chunk(buff, buff->last_chunk);
-        memset(buff->last_chunk, 0, 64);
+    if (buff_chunk_size > 56) {
+        sha256_calc_chunk(buff_last_chunk);
+        memset(buff_last_chunk, 0, 64);
     }
 
     /* Add total size as big-endian int64 x8 */
-    uint64_t size = buff->data_size * 8;
+    int size = buff_data_size * 8;
     int i;
     #pragma hls_unroll yes
     for (i = 8; i > 0; --i) {
-        buff->last_chunk[55+i] = size & 255;
+        buff_last_chunk[55+i] = size & 255;
         size >>= 8;
     }
 
-    sha256_calc_chunk(buff, buff->last_chunk);
+    sha256_calc_chunk(buff_last_chunk);
 }
 
-void sha256_read(const struct sha256_buff* buff, uint8_t* hash) {
-    uint32_t i;
+void sha256_read(char* hash) {
+    int i;
     #pragma hls_unroll yes
     for (i = 0; i < 8; i++) {
-        hash[i*4] = (buff->h[i] >> 24) & 255;
-        hash[i*4 + 1] = (buff->h[i] >> 16) & 255;
-        hash[i*4 + 2] = (buff->h[i] >> 8) & 255;
-        hash[i*4 + 3] = buff->h[i] & 255;
+        hash[i*4] = (buff_h[i] >> 24) & 255;
+        hash[i*4 + 1] = (buff_h[i] >> 16) & 255;
+        hash[i*4 + 2] = (buff_h[i] >> 8) & 255;
+        hash[i*4 + 3] = buff_h[i] & 255;
     }
 }
 
-// static void bin_to_hex(const void* data, uint32_t len, char* out) {
+// static void bin_to_hex(const void* data, int len, char* out) {
 //     static const char* const lut = "0123456789abcdef";
-//     uint32_t i;
+//     int i;
 //     for (i = 0; i < len; ++i){
-//         uint8_t c = ((const uint8_t*)data)[i];
+//         char c = ((const char*)data)[i];
 //         out[i*2] = lut[c >> 4];
 //         out[i*2 + 1] = lut[c & 15];
 //     }
 // }
 
 // void sha256_read_hex(const struct sha256_buff* buff, char* hex) {
-//     uint8_t hash[32];
+//     char hash[32];
 //     sha256_read(buff, hash);
 //     bin_to_hex(hash, 32, hex);
 // }
 
-void sha256_easy_hash(const void* data, size_t size, uint8_t* hash) {
-    struct sha256_buff buff;
-    sha256_init(&buff);
-    sha256_update(&buff, data, size);
-    sha256_finalize(&buff);
-    sha256_read(&buff, hash);
-}
+// void sha256_easy_hash(const void* data, int size, char* hash) {
+//     struct sha256_buff buff;
+//     sha256_init(&buff);
+//     sha256_update(&buff, data, size);
+//     sha256_finalize(&buff);
+//     sha256_read(&buff, hash);
+// }
 
 // void sha256_easy_hash_hex(const void* data, size_t size, char* hex) {
-//     uint8_t hash[32];
+//     char hash[32];
 //     sha256_easy_hash(data, size, hash);
 //     bin_to_hex(hash, 32, hex);
 // }
 
 #pragma hls_top
-int entrypoint(uint8_t a, uint8_t b) {
-    size_t size = 2;
-    uint8_t data[2] = {a, b};
-    struct sha256_buff buff;
-    sha256_init(&buff);
-    sha256_update(&buff, data, size);
-    sha256_finalize(&buff);
-    sha256_read(&buff, hash);
+char entrypoint(char a, char b) {
+    int size = 2;
+    char data[2] = {a, b};
+    char hash[32];
+    sha256_init();
+    sha256_update(data, size);
+    sha256_finalize();
+    sha256_read(hash);
+    return hash[0];
 }
--- projects/sha256/sha256.h
+++ projects/sha256/sha256.h
@@ -30,32 +30,32 @@
 // #include <stddef.h>
 
 struct sha256_buff {
-    uint64_t data_size;
-    uint32_t h[8];
-    uint8_t last_chunk[64];
-    uint8_t chunk_size;
+    int data_size;
+    int h[8];
+    char last_chunk[64];
+    char chunk_size;
 };
 
-/* Initialization, must be called before any further use */
-void sha256_init(struct sha256_buff* buff);
+// /* Initialization, must be called before any further use */
+// void sha256_init(struct sha256_buff* buff);
+// 
+// /* Process block of data of arbitary length, can be used on data streams (files, etc) */
+// void sha256_update(struct sha256_buff* buff, const void* data, size_t size);
+// 
+// /* Produces final hash values (digest) to be read
+//    If the buffer is reused later, init must be called again */
+// void sha256_finalize(struct sha256_buff* buff);
+// 
+// /* Read digest into 32-byte binary array */
+// void sha256_read(const struct sha256_buff* buff, char* hash);
 
-/* Process block of data of arbitary length, can be used on data streams (files, etc) */
-void sha256_update(struct sha256_buff* buff, const void* data, size_t size);
-
-/* Produces final hash values (digest) to be read
-   If the buffer is reused later, init must be called again */
-void sha256_finalize(struct sha256_buff* buff);
-
-/* Read digest into 32-byte binary array */
-void sha256_read(const struct sha256_buff* buff, uint8_t* hash);
-
-int entrypoint(uint8_t a, uint8_t b);
+char entrypoint(char a, char b);
 
 // /* Read digest into 64-char string as hex (without null-byte) */
 // void sha256_read_hex(const struct sha256_buff* buff, char* hex);
 // 
 // /* Hashes single contiguous block of data and reads digest into 32-byte binary array */
-// void sha256_easy_hash(const void* data, size_t size, uint8_t* hash);
+// void sha256_easy_hash(const void* data, size_t size, char* hash);
 // 
 // /* Hashes single contiguous block of data and reads digest into 64-char string (without null-byte) */
 // void sha256_easy_hash_hex(const void* data, size_t size, char* hex);
